2eb6e2ee4ae1bc8a731ba4c65596ccc890c4ade9
Matheus <matheus.dev23@gmail.com>
2023-09-27 14:12:25 -0300

    Entrega do desafio ::tada::

---
+++ index.js
@@ -0,0 +1,21 @@
+let Nome = "Matheus"
+let Xp = 10001;
+let Nivel = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
+
+    if (Xp < 1000){
+      console.log ("O Herói de nome " + Nome + " está no nível de " + Nivel[0])
+  } else if (Xp < 2001){
+      console.log ("O Herói de nome " + Nome + " está no nível de " + Nivel[1])
+  } else if (Xp < 5001){
+      console.log ("O Herói de nome " + Nome + " está no nível de " + Nivel[2])
+  } else if (Xp < 7001){
+      console.log ("O Herói de nome " + Nome + " está no nível de " + Nivel[3])
+  } else if (Xp < 8001){
+      console.log ("O Herói de nome " + Nome + " está no nível de " + Nivel[4])
+  } else if (Xp < 9001){
+      console.log ("O Herói de nome " + Nome + " está no nível de " + Nivel[5])
+  } else if (Xp < 10001){
+      console.log ("O Herói de nome " + Nome + " está no nível de " + Nivel[6])
+  } else if (Xp >= 10001){
+      console.log ("O Herói de nome " + Nome + " está no nível de " + Nivel[7])
+  }
